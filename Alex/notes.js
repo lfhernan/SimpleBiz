858830a85e81fb39a38a054b331411bf52a4bb2b
Alex Comas <comasrent@yahoo.com>
2019-01-27 20:42:01 -0500

    added initial tutorial

---
+++ Alex/notes.js
@@ -0,0 +1,30 @@
+// if(condition){
+//     //stuff to do if condition is true
+// }
+
+/*
+    if(condition){
+        // do something
+    }
+    else{
+        // do something else
+    }
+
+    if(condition){
+
+    }
+    else if(other condition){
+        
+    }
+*/
+
+// operators == != < > <= >= &&
+
+
+/*
+ falsy values
+   undefined
+   null
+   ""
+   []
+*/
