190c33b928ebf1985ad97a14905b57dccd35c819
Help <pczerokey@gmail.com>
2019-02-11 10:11:42 -0500

    up to 26:05

--- client/src/Martyn/Navbar.js
+++ client/src/Martyn/Navbar.js
@@ -1,7 +1,31 @@
 import React, { Component } from 'react';
 
 class Navbar extends Component {
+
+    constructor(props)
+    {
+        super(props);
+
+        this.state = {
+            loggedIn: false
+        }
+    }
+
   render() {
+
+    let navItems = 
+    <ul className="navbar-nav">
+        <li className="nav-item active">
+            <a className="nav-link" href="#">Home<span className="sr-only">(current)</span></a>
+        </li>
+        <li className="nav-item">
+            <a className="nav-link" href="#">Account</a>
+        </li>
+        <li className="nav-item">
+            <button className="nav-link" onClick={this.props.toggleLogIn} href="#">Logout</button>
+        </li>
+    </ul>
+
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <a className="navbar-brand" href="#">Box World</a>
@@ -10,17 +34,7 @@ class Navbar extends Component {
         </button>
 
         <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-                <li className="nav-item active">
-                    <a className="nav-link" href="#">Home<span className="sr-only">(current)</span></a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Account</a>
-                </li>
-                <li className="nav-item">
-                    <button className="nav-link" onClick={this.props.toggleLogIn} href="#">Logout</button>
-                </li>
-            </ul>
+            {navItems}
         </div>
 
         </nav>
