35cbe7572dd32fdfaec44ae71da34b1e82b677cc
Daniel Valencia <valenciad1994@gmail.com>
2019-02-20 13:46:43 -0500

    added single employee to the employee list

---
+++ client/src/components/EmployeesList/Employee.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react'
+
+class Employee extends Component{
+    render(){
+        return (
+            <div>
+                Single Employee
+            </div>
+        )
+    }
+}
+
+export default Employee
--- client/src/components/EmployeesList/EmployeesList.js
+++ client/src/components/EmployeesList/EmployeesList.js
@@ -1,10 +1,14 @@
 import React,{Component} from 'react'
+import Employee from './Employee'
 
 class EmployeesList extends Component{
     render(){
         return (
             <div>
                 List of Employees
+                <Employee />
+                <Employee />
+                <Employee />
             </div>
         )
     }
