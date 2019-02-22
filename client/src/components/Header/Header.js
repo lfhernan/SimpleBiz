dbe4fd8bcdfeb151082272fdfcc7439a360796d2
Help <pczerokey@gmail.com>
2019-02-22 12:49:36 -0500

    Added carousel, missing tint

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,42 +1,35 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import { UncontrolledCarousel } from 'reactstrap';
 
-const styles = {
-    height: '100%!' ,
-    width: '100%!',
-    backgroundImage: 'url("http://1.bp.blogspot.com/-cXzhLJTSrFM/VmMEeCCE_5I/AAAAAAAACHc/3bjbi56-AoM/s1600/a-crosswalk-on-a-busy-street-new-york-city-united-states1152_12837314014-tpfil02aw-31089-1.jpeg")',
-    /* Center and scale the image nicely */
-    backgroundPosition: 'center!',
-    backgroundRepeat: 'no-repeat!',
-    backgroundSize: 'contain!',
-    display: 'inline!',
-    position: 'relative!'
-}
+const items = [
+  {
+    src: 'https://www.epichotel.com/images/1700-960/downtown-miami-header-255095f7.jpg',
+    altText: 'Slide 1',
+    caption: 'This is where your business grows',
+    header: 'Big boys club'
+  },
+  {
+    src: 'https://thenypost.files.wordpress.com/2018/08/asians_scene_party1a.jpg?quality=90&strip=all',
+    altText: 'Slide 2',
+    caption: 'happy and cheerful',
+    header: 'The Good Life'
+  },
+  {
+    src: 'https://teamkgsr.com/wp-content/uploads/2018/11/sucesfull-business.jpg',
+    altText: 'Slide 3',
+    caption: 'Instant Results',
+    header: 'Achieve Success Here'
+  }
+];
 
-const text = {
-    textAlign: 'center'
-}
-
-const overlay = {
-    zIndex: '1',
-    height: '100%',
-    width: '100%',
-    position: 'fixed',
-    overflow: 'auto',
-    background: 'rgba(0, 0, 0, 0.0)' /*can be anything, of course*/
-}
 
 class Header extends Component 
 {
   render() 
   {
     return (
-      <div className="App-Header-Overlay" style={overlay}>
-        <div className="App-Header" style={styles}>
-            <div className='App-Header-test' style={text}>
-                <h3>CTA</h3>
-                <p>this is just test</p>
-            </div>
-        </div>
+      <div className='overlay'>
+        <UncontrolledCarousel items={items}/>
       </div>
     );
   }
