9a0f5e4aac4edbb90f50433a41a23e50d60d0be8
Jessica <jmore178@fiu.edu>
2019-02-25 13:10:25 -0500

    final using reactstrap

--- client/src/components/Essential/AboutHome.js
+++ client/src/components/Essential/AboutHome.js
@@ -1,14 +1,15 @@
 import React,{Component} from 'react'
-import { Button } from 'reactstrap';
+import { Jumbotron, Button } from 'reactstrap';
 
 
 class AboutHome extends Component{
     render(){
         return (
-                <div style={{backgroundColor: '#D3D3D3', margin: '10px'}}>
-                    <center>
-                        <h1>About</h1>
-                        <p style={{textAlign: 'justify', paddingLeft: '20px', paddingRight: '20px'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                <div>
+                    <Jumbotron style={{background: 'none'}}>
+                    
+                        <h1 className="display-3">About</h1>
+                        <p className='lead'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                             Duis condimentum justo ut tellus convallis, vitae sollicitudin eros euismod. 
                             Nulla vitae dignissim mauris, interdum posuere quam. 
                             In tristique tellus lectus, in sodales sapien pellentesque in. 
@@ -20,13 +21,12 @@ class AboutHome extends Component{
                             scelerisque lorem. Suspendisse finibus justo eget felis dapibus ultrices. 
                             Sed eleifend purus augue, ut fermentum metus efficitur eu. Aliquam porttitor 
                             pretium lectus, non scelerisque neque auctor at. Sed ac pulvinar purus. </p>
-                    </center>
-                        <div style={{backgroundColor: 'grey'}}>
-                            <center>
-                                <a color= 'white' href='http://localhost:3000/SignUp'><Button color = 'success'> Get Started Now!</Button></a>{''}
-                            </center>
-                </div>    
-                        </div>                            
+                        <hr className="my-2" />
+                        <p>
+                            <a color= 'white' href='http://localhost:3000/SignUp'><Button color = 'success'> Get Started Now!</Button></a>{''}
+                        </p>
+                    </Jumbotron>
+                </div>                              
         )
     }
 }
