13519007cbaaabc9d675bdb28c6eafeac2642e55
Alex Comas <newadmin@Alex-Computer.local>
2019-02-25 17:27:52 -0500

    fixed carousel, note: images must be of same size

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -23,13 +23,13 @@ const items = [
     header: 'Big boys club'
   },
   {
-    src: 'https://thenypost.files.wordpress.com/2018/08/asians_scene_party1a.jpg?quality=90&strip=all',
+    src: 'https://www.epichotel.com/images/1700-960/downtown-miami-header-255095f7.jpg',
     altText: 'Slide 2',
     caption: 'happy and cheerful',
     header: 'The Good Life'
   },
   {
-    src: 'https://teamkgsr.com/wp-content/uploads/2018/11/sucesfull-business.jpg',
+    src: 'https://www.epichotel.com/images/1700-960/downtown-miami-header-255095f7.jpg',
     altText: 'Slide 3',
     caption: 'Instant Results',
     header: 'Achieve Success Here'
