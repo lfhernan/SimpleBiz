8700a095caae6423249e0d6f3ec928c9e0b950b6
Help <pczerokey@gmail.com>
2019-02-26 10:10:47 -0500

    Fixed naming convenction for componentInfo to ComponentInfo, restructured file hierarchy

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,22 +1,15 @@
-import React,{Component} from 'react'
+import React,{Component, Fragment} from 'react'
 import { Container, Row, Col } from 'reactstrap';
-<<<<<<< HEAD
-import AboutHome from '../components/Essential/AboutHome';
-
-class Home extends Component{
-    render(){
-        return (
-            <div>
-                <AboutHome />
-            </div> 
-=======
 import Header from '../components/Header/Header'
+import AboutHome from '../components/Essential/AboutHome/AboutHome'
 class Home extends Component{
     render(){
         return (
+            <Fragment>
+                <Header />
+                <AboutHome />
+            </Fragment>
             // <div>Home</div>
-            <Header />
->>>>>>> master
         )
     }
 }
