c81b2e5e24fc29bc89157b6e6cfb2c76dfe089ac
Daniel Valencia <valenciad1994@gmail.com>
2019-02-27 12:06:05 -0500

    added template for single employee display

--- client/src/components/EmployeesList/Employee.js
+++ client/src/components/EmployeesList/Employee.js
@@ -1,11 +1,50 @@
 import React,{Component} from 'react'
-import { Col, ListGroupItem } from 'reactstrap';
+import { Col, Row, ListGroupItem } from 'reactstrap';
 
 class Employee extends Component{
+    
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+            employeeId: '',
+            name: '',
+            position: ''
+        };
+    }
+     
     render(){
+
+        const image = {
+            width: '150px',
+            borderRadius: '50%',
+            float: 'left',
+            margin: '10px'
+        }
+
         return (
             <ListGroupItem>
-                Single Employee 
+                <Row>
+                    <Col md="3">
+                        <img src="https://images.unsplash.com/photo-1518548183878-5e1fcea68bb9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+                            alt="employee image" style={image}
+                        />   
+                        
+                        <div>
+                            <p>Name</p>
+                            <p>Position</p>    
+                        </div>
+                    </Col> 
+
+                    {/* Space between the employee info and toolbar */}
+                    <Col md="6" />
+
+                    <Col md="3">
+                        <div>
+                            Modal   | Edit  | Remove
+                        </div>    
+                    </Col>
+                </Row>
             </ListGroupItem>
         )
     }
--- client/src/components/EmployeesList/EmployeesList.js
+++ client/src/components/EmployeesList/EmployeesList.js
@@ -3,10 +3,26 @@ import { ListGroup} from 'reactstrap';
 import Employee from './Employee';
 
 class EmployeesList extends Component {
+    
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+            employeeId: '',
+            name: '',
+            position: ''
+        };
+    }
+    
     render() {
         return (
             <ListGroup>
                 List of Employees
+                <Employee/>
+                <Employee/>
+                <Employee/>
+                <Employee/>
+                <Employee/>
                 <Employee/>
             </ListGroup>
         );
