61614c20b9f4903143bd7f559c7795f7276c460e
Alex Comas <newadmin@Alex-Computer.local>
2019-03-04 09:41:02 -0500

    fixed a conflict in home page

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,31 +1,19 @@
-<<<<<<< HEAD
-import React,{Component} from 'react'
-import { Container, Row, Col } from 'reactstrap'
+import React,{Component,Fragment} from 'react'
+// import { Container, Row, Col } from 'reactstrap'
 import Header from '../components/Header/Header'
 import BenefitView from '../components/BenefitView/BenefitView'
+import AboutHome from '../components/Essential/AboutHome/AboutHome'
+
 
 class Home extends Component{
     render(){
         return (
-            <Container>
-                <Header />
+            <Fragment>
+                 <Header />
                 <div>Home</div>
                 <BenefitView />
-            </Container>
-=======
-import React,{Component, Fragment} from 'react'
-import { Container, Row, Col } from 'reactstrap';
-import Header from '../components/Header/Header'
-import AboutHome from '../components/Essential/AboutHome/AboutHome'
-class Home extends Component{
-    render(){
-        return (
-            <Fragment>
-                <Header />
                 <AboutHome />
             </Fragment>
-            // <div>Home</div>
->>>>>>> master
         )
     }
 }
